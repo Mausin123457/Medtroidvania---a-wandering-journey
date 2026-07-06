1918b2dcdb1c162a228c2964cace597ddd3aef73
Maurice Sinon <166910341+Mausin123457@users.noreply.github.com>
2026-07-06 18:56:56 +0200

    I am back and will be working on a save and load system!

--- General/PlayerSpawn/PlayerSpawn.gd
+++ General/PlayerSpawn/PlayerSpawn.gd
@@ -6,7 +6,13 @@ func _ready() -> void:
 	visible = false
 	await get_tree().process_frame
 	
+	if get_tree().get_first_node_in_group("Player"):
+		return
+		
+	var player: Player = preload("uid://cj1d8xaw4i7jr").instantiate()
+	get_tree().root.add_child(player)
 	
+	player.global_position = self.global_position
 	
 	
 	pass
